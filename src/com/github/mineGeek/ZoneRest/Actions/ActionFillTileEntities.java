4274fb531692d55ddfd3aa4acfe4ea4a7924bef4
Moí <samuel@sweetsquared.com>
2013-02-21 23:54:17 +0000

    WiP

--- src/com/github/mineGeek/ZoneRest/Actions/ActionFillTileEntities.java
+++ src/com/github/mineGeek/ZoneRest/Actions/ActionFillTileEntities.java
@@ -1,39 +1,69 @@
 package com.github.mineGeek.ZoneRest.Actions;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.Chest;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
+import com.github.mineGeek.ZoneReset.Data.Area;
 import com.github.mineGeek.ZoneReset.Data.ZItem;
+import com.github.mineGeek.ZoneReset.Data.Zones;
+import com.github.mineGeek.ZoneReset.Utilities.Utilities;
 import com.github.mineGeek.ZoneReset.ZoneReset.ZRScope;
 
 public class ActionFillTileEntities extends Action {
 	
 	public List<ItemStack> items = new ArrayList<ItemStack>();
-	public List<BlockState>entities = new ArrayList<BlockState>();
+	public List<Map<String, Object>> entities = new ArrayList<Map<String, Object>>();
 	public ZRScope scope = ZRScope.REGION;
 
 	
 	public void run() {
 		
-		
+		if ( !entities.isEmpty() ) {
+			
+			for ( Map<String, Object> ent : entities ) {
+				BlockState bs = getBlockState( ent );
+				if ( ent != null ) {
+					
+					Inventory inv = null;
+					
+					if ( bs instanceof Chest ) {
+						inv = ((Chest)bs).getBlockInventory();				
+					} else  {
+						inv = ((InventoryHolder)bs).getInventory();
+					}
+					
+					for ( ItemStack i : items ) {
+						inv.addItem(i);
+					}					
+					
+				}
+			}
+			
+		}
 		
 		
 	}
-
 
 	@Override
 	public void setToConfig(String root, ConfigurationSection c) {
 		
 		if ( !enabled ) return;
 		
-		if ( !scope.equals( ZRScope.REGION ) ) c.set( root + ".containers.add.scope", scope.toString().toLowerCase() );
+		//if ( !scope.equals( ZRScope.REGION ) ) c.set( root + ".containers.add.scope", scope.toString().toLowerCase() );
 		
 		List<Map<String, Object>> a = new ArrayList<Map<String, Object>>();
 		
@@ -47,22 +77,66 @@ public class ActionFillTileEntities extends Action {
 		
 		List<Map<String, Object>> b = new ArrayList<Map<String, Object>>();
 		
-		for ( BlockState bs : entities ) {
-			
-			Map<String, Object> e = new HashMap<String, Object>();
-			List<Integer> xyz = new ArrayList<Integer>();
-			xyz.addAll( Arrays.asList( bs.getLocation().getBlockX(), bs.getLocation().getBlockY(), bs.getLocation().getBlockZ()));
-			e.put("world",  bs.getLocation().getWorld().getName() );
-			e.put("xyz", xyz );
-			b.add( e );
+		for ( Map<String, Object> map : entities ) {
+				
+				BlockState bs = this.getBlockState( map );
+				if ( bs != null) b.add( map );			
 		}
 		
 		if ( !b.isEmpty() ) c.set( root + ".containers.add.to", b );
 		
 	}
 
+	
+	public List<BlockState> getList() {
+		
+		List<BlockState> bs = new ArrayList<BlockState>();
+		List<Chunk> chunks = null;
+		
+		if ( this.scope.equals( ZRScope.REGION ) ) {
+			
+			chunks = Utilities.getChunksFromArea( Zones.getZone( this.tag ).getArea() );
+
+		}
+		
+		Area a = Zones.getZone( this.tag ).getArea();
+		
+		for ( Chunk c : chunks ) {
+			
+			for ( BlockState b : c.getTileEntities() ) {
+				if ( a.intersectsWith( b.getLocation() ) ) {
+					bs.add( b );
+				}
+			}
+			
+		}
+		
+		return bs;
+		
+	}
+	
 
 	@SuppressWarnings("unchecked")
+	private BlockState getBlockState( Map<String, Object> map ) {
+		
+		World w = null;
+		Location l = null;
+		List<Integer> xyz = null;
+		Block block = null;
+		BlockState bs = null;
+		
+		if ( map.containsKey( "world") && map.containsKey("xyz") ) {
+			w = Bukkit.getWorld( (String) map.get("world") );
+			if ( w != null ) xyz = (List<Integer>) map.get("xyz");
+			if ( xyz != null ) l = new Location( w, xyz.get(0), xyz.get(1), xyz.get(2));
+			if ( l != null )block = w.getBlockAt( l );
+			if ( block != null ) bs = block.getState();				
+		}
+		return bs;
+		
+	}
+	
+	@SuppressWarnings({ "unchecked" })
 	@Override
 	public void loadFromConfig(String root, ConfigurationSection c) {
 		
@@ -86,13 +160,13 @@ public class ActionFillTileEntities extends Action {
 			
 			if ( !items.isEmpty() ) {
 				for ( Map<?,?> map : a ) {
-					items.add(  );
+					entities.add( (Map<String, Object>) map );
 				}
 			}
 			
 		}
 		
-		if ( !b.isEmpty() ) c.set( root + ".containers.add.to", b );		
+		if ( !entities.isEmpty() ) c.set( root + ".containers.add.to", entities );		
 		
 	}
 
--- src/com/github/mineGeek/ZoneRest/Actions/ActionMovePlayers.java
+++ src/com/github/mineGeek/ZoneRest/Actions/ActionMovePlayers.java
@@ -1,9 +1,13 @@
 package com.github.mineGeek.ZoneRest.Actions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 import com.github.mineGeek.ZoneReset.Data.Zones;
@@ -13,38 +17,66 @@ import com.github.mineGeek.ZoneReset.ZoneReset.ZRScope;
 public class ActionMovePlayers extends Action {
 
 	public ZRScope scope = ZRScope.REGION;
-	public String worldName;
-	public int toX;
-	public int toY;
-	public int toZ;
-	
+	public Location location = null;
+
 	public void run() {
 		
-		Location l = new Location( Bukkit.getWorld( this.worldName ), toX, toY, toZ );
+		if ( location == null || !enabled ) return;
 		
 		if ( this.scope.equals( ZRScope.REGION ) ) {
 			
 			List<String> players = Utilities.getPlayersNearZone( Zones.getZone( this.tag ) );
 			for ( String x : players ) {
-				if ( Bukkit.getPlayer(x).isOnline() ) Bukkit.getPlayer(x).teleport(l);
+				if ( Bukkit.getPlayer(x).isOnline() ) Bukkit.getPlayer(x).teleport(location);
 			}
 		} else if ( this.scope.equals( ZRScope.WORLD ) ) {
 			
 			List<Player> players = Bukkit.getWorld( Zones.getZone( this.tag ).getWorldName() ).getPlayers();
 			for ( Player p : players ) {
-				if ( p.isOnline() ) p.teleport(l);
+				if ( p.isOnline() ) p.teleport(location);
 			}
 			
 		} else if ( this.scope.equals( ZRScope.SERVER ) ) {
 			Player[] players = Bukkit.getServer().getOnlinePlayers();
 			for ( Player player : players ) {
-				player.teleport(l);
+				player.teleport(location);
 			}
 		}
 		
 	}
 	
 	public void close() {
+		this.location =  null;
+	}
+
+	@Override
+	public void setToConfig(String root, ConfigurationSection c) {
+		
+		if ( !enabled ) return;
+		
+		if ( !scope.equals( ZRScope.REGION ) ) c.set( root + ".moveplayers.scope", scope.toString().toLowerCase() );
+		
+		if ( location != null ) {
+			c.set( root + ".moveplayers.world", location.getWorld().getName() );
+			c.set( root + ".moveplayers.xyz", new ArrayList<Integer>( Arrays.asList( location.getBlockX(), location.getBlockY(), location.getBlockZ() ) ) );			
+		}
+			
+		
+	}
+
+	@Override
+	public void loadFromConfig(String root, ConfigurationSection c) {
+
+		scope = ZRScope.valueOf( c.getString( root + ".moveplayers.scope", "region").toUpperCase() );
+		String worldName = Zones.getZone( this.tag ).getWorldName();
+		
+		worldName = c.getString( root + ".moveplayers.world", worldName );
+		
+		if ( c.isSet( root + ".moveplayers.xyz" ) ) {
+			List<Integer> xyz = c.getIntegerList( root + ".moveplayers.xyz" );
+			location = new Location( Bukkit.getWorld( worldName ), xyz.get(0), xyz.get(1), xyz.get(2) );
+		}		
+		enabled = ( !scope.equals( ZRScope.REGION ) || location != null );
 		
 	}
 	
